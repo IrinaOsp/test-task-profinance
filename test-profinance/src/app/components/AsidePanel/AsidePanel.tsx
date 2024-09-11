a9451832b73eecf68d88b702faa23f3161c4401f
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-09-11 15:44:50 +0200

    feat: complete aside panel

--- test-profinance/src/app/components/AsidePanel/AsidePanel.tsx
+++ test-profinance/src/app/components/AsidePanel/AsidePanel.tsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
 import ContactsCard from "../ContactsCard/ContactsCard";
 import ControlCard from "../ControlCard/ControlCard";
+import { buttonVariants } from "@/components/ui/button";
+import { MessageCircleMore } from "lucide-react";
 
 export default function AsidePanel() {
   return (
-    <aside>
-      AsidePanel
+    <aside className="m-3 flex flex-col gap-3">
       <ControlCard />
       <ContactsCard />
+      <Link
+        href={"mailto:pf1@werthesest.ru"}
+        className={
+          buttonVariants({ variant: "default" }) + "w-full rounded-2xl"
+        }
+        style={{ backgroundColor: "#2563eb" }}
+      >
+        <MessageCircleMore className="mr-1 size-4" />
+        Связаться с нами
+      </Link>
     </aside>
   );
 }
